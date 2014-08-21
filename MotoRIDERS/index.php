097b86eee041b42426ccc12cd47e2745c9cfcad3
marlon230496 <marlon230496@hotmail.com>
2014-08-20 23:18:31 -0300

    Projeto desenvolvimento site em php

---
+++ MotoRIDERS/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>MotoRIDERS</title>
+<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="./css/estilo.css">
+</head>
+<body>
+<!-- Topo Site -->
+	<div id="topo">
+		<h1>MotoRIDERS</h1>
+	</div>
+<!-- Fim Topo Site -->
+
+<!-- Menu Site -->
+	<div id="menu">
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Quem Somos</a></li>
+			<li><a href="#">Catálogo</a></li>
+			<li><a href="#">Contato</a></li>
+						
+		</ul>
+	</div>
+<!-- Fim Menu Site -->
+
+<!-- Aqui começa o conteúdo da página -->
+	<div id="centro">
+	
+		<h1>Tech Solutions</h1>
+		<p>Essa página destina-se para execução e apresentação do sitema de ordens de serviços</p>
+	
+	</div>
+<!-- Fim Conteúdo do Site -->
+
+<!-- Rodape -->	
+	<div id="rodape">
+		<p>by MarlonSilva</p>
+	</div>
+<!-- Fim Rodape -->	
+
+</body>
+</html>
